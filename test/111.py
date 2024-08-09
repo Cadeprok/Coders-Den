007af8ae1f2d56c776d2f9c0d5dcb179c9d86b58
Cadeprok <prokopc1@montclair.edu>
2024-08-08 21:54:07 -0400

    -

--- test/111.py
+++ test/111.py
@@ -61,3 +61,71 @@ def get_data():
           }
           })  
         }
+    
+
+
+
+
+
+
+
+    @app.route('/api/get_data/<user_id>', methods=['GET'])
+def get_data(user_id):
+    user = load_user(user_id)
+    print('hello')
+    data = {
+        "ownsPython": str(user.javascriptOwner),
+        "ownsJava" : str(user.pythonOwner),
+        "ownsPython" : str(user.javaOwner)
+    }
+        #"ownsPython": str(User.query.filter_by(user.username).first().javascriptOwner),
+        #"ownsJava" : str(User.query.filter_by(user.username).first().pythonOwner),
+        #"ownsPython" : str(User.query.filter_by(user.username).first().ownsJavascript)
+    return jsonify(data)
+
+
+
+
+
+
+
+
+
+    try:
+        form = loginForm()  
+        username = form.username.data
+        
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            raise ValueError("User not found")
+        
+
+        data = {
+            "ownsPython": str(user.pythonOwner),
+            "ownsJava": str(user.javaOwner),  # Fixed key
+            "ownsJavascript": str(user.ownsJavascript)
+        }
+        
+        return jsonify(data)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
+    
+
+
+
+
+
+
+
+                /*if(value){
+              url = `{{ ${user_id}/${getPurchasing(key)} }}`
+              buttonText += 'Learn'
+            }
+            else{
+              url = `{{ ${user_id}/shoppingCart(${key}) }}`
+              buttonText += `Purchase`
+            }
+            javascriptOwner = db.Column(db.Boolean, default=False, nullable=False)
+            pythonOwner = db.Column(db.Boolean, default=False, nullable=False)
+            javaOwner = db.Column(db.Boolean, default=False, nullable= False)*/
